71662ef1e4ef004a6af9433bc32776245cb90233
Iuliia <badmad_91@inbox.ru>
2022-03-22 14:10:58 +0300

    Updated class

--- src/com/laba/animal/Zebra.java
+++ src/com/laba/animal/Zebra.java
@@ -1,6 +1,6 @@
 package com.laba.animal;
 
-public class Zebra extends Herbivores {
+public class Zebra extends Herbivores implements Runable {
 
     private static String description;
 
@@ -9,16 +9,34 @@ public class Zebra extends Herbivores {
     }
 
     static {
-        description = "Zebra is from Africa too.";
+        description = "Zebra is from Africa.";
     }
 
     public static void showDescription() {
         System.out.println(description);
     }
 
+    @Override
+    public void printName(Animal type) {
+        String zebra = type.getAnimalType();
+        System.out.println(zebra + " - description: ");
+    }
+
+    @Override
+    public void showAge(Animal age) {
+        int old = age.getAge();
+        System.out.println("This animal can live up to " + old + " years.");
+    }
+
+    @Override
     public String toString() {
         return ("This animal is a " + getAnimalType() + ".\n" + "He is from " + getCountry() + ".\n" +
                 "Maximum adult weight can be up to " + getMaxWeight() + " kg.");
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Zebra can run!");
     }
 
 }

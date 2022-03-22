e7d7078bb5fcc04b4f7237d24bd34cdfcedfd94d
Iuliia <badmad_91@inbox.ru>
2022-03-22 14:04:56 +0300

    Updated abstract class

--- src/com/laba/animal/Herbivores.java
+++ src/com/laba/animal/Herbivores.java
@@ -1,26 +1,13 @@
 package com.laba.animal;
 
-public class Herbivores extends Animal {
+public abstract class Herbivores extends Animal {
 
     public Herbivores(String animalType, int age, int maxWeight, String country, int maxSpeed) {
         super(animalType, age, maxWeight, country, maxSpeed);
     }
 
-    @Override
-    public void printName(Animal type) {
-        String herbivore = type.getAnimalType();
-        System.out.println(herbivore + " - description: ");
-    }
+    public abstract void printName(Animal type);
 
-    @Override
-    public void showAge(Animal age) {
-        int old = age.getAge();
-        System.out.println("This animal can live up to" + old + " years.");
-    }
-
-    public String toString() {
-        return ("This animal is " + getAnimalType() + ".\n" + "He is from " + getCountry() + ".\n" +
-                "Maximum adult weight can be up to " + getMaxWeight() + " kg.");
-    }
+    public abstract void showAge(Animal age);
 
 }

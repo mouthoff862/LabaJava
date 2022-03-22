2befc46cdab9c9d5bc89cc83ae9a2fe40d75cea0
Iuliia <badmad_91@inbox.ru>
2022-03-22 14:23:47 +0300

    Updated iterface

--- src/com/laba/animal/IFeed.java
+++ src/com/laba/animal/IFeed.java
@@ -3,7 +3,7 @@ package com.laba.animal;
 public interface IFeed {
 
     public default void printDuties() {
-        System.out.println("Zookeepers duties: ");;
+        System.out.println("Zookeeper feed");;
     }
 
 }
